792015abffc4cd07b26c6a58067e7a7a2e459255
Tyler Galvin <tylergalvin97@gmail.com>
2022-02-01 16:51:24 -0500

    js problem sets 1

--- homework.js
+++ homework.js
@@ -1,26 +1,64 @@
 // 1. Set the variable `givenName` to the string "Addison".
+let givenName = 'Addison'
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+let candies = 20
+let people = 6 
+let leftover = 20 % 6
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+function greeting (person) {
+    return 'Hello, ' + person + "!"
+}
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
+function isOdd(num) {
+    if (num % 2 === 0){
+        return false
+    }
+    else if (Number.isInteger(num)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+function isEven (number) {
+    if (!isOdd(number) && Number.isInteger(number)) {
+        return true
+    }else {
+        return false
+    }
+    }
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+function fahrenheitToCelsius (faren) {
+ let celsius = (faren - 32) * 5/9
+ return celsius 
+}
+
+// (50°F − 32) × 5/9 = answer
+
 
 // 7. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+function celsiusToFahrenheit (celsius) {
+    let fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+   }
+
+// (0°C × 9/5) + 32 = answer
 
 // 8. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -28,10 +66,25 @@
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
+function fahrenheitToKelvin (f) {
+    // let kelvin = ((f - 32) * 5/9) + 273.15
+    let kelvin = fahrenheitToCelsius(f) + 273.15
+    return kelvin
+}
+
+
+// (32°F − 32) × 5/9 + 273.15 = answer
 
 // 9. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+function lesser (number1, number2) {
+    if (number1 < number2) {
+        return number1
+    } else {
+        return number2
+    }
+}
 
 // 10. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -44,6 +97,20 @@
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
+function multigreeting(name, language) {
+    if (language == 'en') {
+        return 'Hello, ' + name + '!'
+    }
+    if (language == 'es') {
+        return '¡Hola, ' + name + '!'
+    }
+    if (language == 'fr') {
+        return 'Bonjour, ' + name + '!'
+    }
+    if (language == 'eo') {
+        return 'Saluton, ' + name + '!'
+    }
+}
 
 // 11. The greatest common divisor (https://en.wikipedia.org/wiki/Greatest_common_divisor)
 // is the largest integer that, given two other integers, can be divided into them. For
